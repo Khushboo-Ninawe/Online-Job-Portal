3ede712d1face00673ee18a0be451175c68d1229
khushbooninawe <khushbooninawe24@gmail.com>
2024-10-18 11:35:44 +0530

    Initial commit

---
+++ OnlineJobPortal/System/constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = 'America/New_York';
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = '';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+?>
